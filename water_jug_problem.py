90cb08a097e5e4ac72f33e07e4a049ebd3aa63e2
Samet Salgın <sametsalgin10@gmail.com>
2019-10-23 23:03:53 +0300

    Create water_jug_problem.py
    A Water Jug Problem: You are given two jugs, a 4-gallon one and a 3-gallon one, a pump which has unlimited water which you can use to fill the jug, and the ground on which water may be poured. Neither jug has any measuring markings on it. How can you get exactly 2 gallons of water in the 4-gallon jug?

---
+++ water_jug_problem.py
@@ -0,0 +1,26 @@
+from collections import defaultdict 
+
+visited = defaultdict(lambda: False) 
+jug1_capacity, jug2_capacity, aim = 4, 3, 2
+
+def waterJugSolver(action,jug1,jug2):
+    if (jug1 == aim): 
+        print("end of the steps",jug1, jug2) 
+        return True
+
+    if visited[(jug1, jug2)] == False: 
+        print(action,jug1, jug2) 
+
+        visited[(jug1, jug2)] = True
+
+        return (waterJugSolver(">Pour jug1<        ",0, jug2) or
+                waterJugSolver(">Pour jug2<        ",jug1, 0) or
+                waterJugSolver(">Fill jug1<        ",jug1_capacity, jug2) or
+                waterJugSolver(">Fill jug2<        ",jug1, jug2_capacity) or
+                waterJugSolver(">From jug2 to jug1<",jug1 + min(jug2, (jug1_capacity-jug1)), jug2 - min(jug2, (jug1_capacity-jug1))) or
+                waterJugSolver(">From jug1 to jug2<",jug1 - min(jug1, (jug2_capacity-jug2)),  jug2 + min(jug1, (jug2_capacity-jug2)))) 
+    else:
+        return False
+
+print("Steps: ") 
+waterJugSolver(">Initial state<    ",0, 0) 
